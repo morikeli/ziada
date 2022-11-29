475f44f613f414369854fd6a619cba5561faa72b
Mori Keli <kelimbth@gmail.com>
2022-11-29 15:39:49 +0300

    updated files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+    id = models.CharField(max_length=18, primary_key=True, unique=True, editable=False)
+    student = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    gender = models.CharField(max_length=7, blank=False)
+    phone_number = models.CharField(max_length=13, blank=False)
+    course = models.CharField(max_length=100, blank=False)
+    school = models.CharField(max_length=70, blank=False)
+    year = models.CharField(max_length=12, blank=False)
+    semester = models.CharField(max_length=1, blank=False)
+    profile_pic = models.ImageField(upload_to='Students-Dps/', default='default.png')
+    is_student = models.BooleanField(default=False, editable=False)
+    is_prefect = models.BooleanField(default=False, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+class Assignments(models.Model):
+    name = models.ForeignKey(Students, on_delete=models.CASCADE, editable=False)
+    unit = models.CharField(max_length=70, blank=False)
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+from .forms import LoginForm, SignupForm
 
-# Create your views here.
+
+class UserLogin(LoginView):
+    authentication_form = LoginForm
+    template_name = 'users/login.html'
+
+
+def signup_view(request):
+    form = SignupForm()
+
+
+    context = {'signup_form': form}
+    return render(request, 'users/signup.html', context)
+
+def homepage_view(request):
+
+    context = {}
+    return render(request, 'users/homepage.html', context)
+
+
+def userprofile_view(request):
+
+    context = {}
+    return render(request, 'users/profile.html', context)
+
+class LogoutUser(LogoutView):
+    template_name = 'users/logout.html'
