9e81fa4518cc0658079c464250c97b7a3622d140
Mori Keli <kelimbth@gmail.com>
2022-11-29 14:07:10 +0300

    created custom views

--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 
-# Create your views here.
+
+class UserLogin(LoginView):
+    template_name = 'students/login.html'
+
+
+def signup_view(request):
+
+    context = {}
+    return render(request, 'students/signup.html', context)
+
+def homepage_view(request):
+
+    context = {}
+    return render(request, 'students/homepage.html', context)
+
+
+class LogoutUser(LogoutView):
+    template_name = 'students/logout.html'
