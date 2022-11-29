5748165e5a009486eaf15cab587858d941f0db17
Mori Keli <kelimbth@gmail.com>
2022-11-29 16:23:52 +0300

    created signals

---
+++ users/signals.py
@@ -0,0 +1,32 @@
+from django.db.models.signals import pre_save, post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Students, Assignments, Lecturers
+import uuid
+
+
+@receiver(pre_save, sender=Students)
+def generate_StudentId(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:18]
+
+@receiver(pre_save, sender=Assignments)
+def generate_StudentId(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:18]
+
+@receiver(pre_save, sender=Lecturers)
+def generate_StudentId(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:18]
+
+
+@receiver(pre_save, sender=Students)
+def create_profile(sender, instance, **kwargs):
+    if instance.is_student is False and instance.is_prefect is False:
+        @receiver(post_save, sender=User)
+        def create_student_profile(sender, instance, created, *kwargs):
+            if created:
+                if instance.is_staff is False and instance.is_superuser is False:
+                    Students.objects.create(student=instance)
+
