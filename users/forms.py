1b4e754007126daa6a7bdbda7c077a44a68223ae
Mori Keli <kelimbth@gmail.com>
2022-11-30 07:08:02 +0300

    created editprofile & updateprofile forms

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UsernameField
 from django.contrib.auth.models import User
 from django import forms
+from .models import Students, Assignments, Lecturers
 
 
 class LoginForm(AuthenticationForm):
@@ -20,4 +21,53 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    CHOICE_GENDER = (
+        (None, '-- Select your gender --'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    CHOICE_COURSE = (
+        (None, '-- Select course --'),
+        ('Bsc. in Computer Science', 'Bsc. in Computer Science'),
+
+    )
+    CHOICE_SCHOOL = (
+        (None, '-- Select your school --'),
+        ('School of Arts, Social Sciences and Business', 'School of Arts, Social Sciences and Business (SASSB)'),
+        ('School of Education', 'School of Education (SE)'),
+        ('School of Information, Communication & Media Studies', 'School of Information, Communication & Media Studies (INFOCOMS)'),
+        ('School of Science, Agriculture & Environmental Science', 'School of Science, Agriculture & Environmental Science (SSAES)'),
+    )
+    CHOICE_YEAR = (
+        (None, '-- Year of study --'),
+        ('First year', 'First year (Fresher)'),
+        ('Second year', 'Second year (Sophomore)'),
+        ('Third year', 'Third year (Junior)'),
+        ('Fourth year', 'Fourth year (Senior)'),
+    )
+    CHOICE_SEMESTER = (
+        (None, '-- Select semester --'),
+        ('1', '1'),
+        ('2', '2'),
+    )
+
+    gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=CHOICE_GENDER)
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'placeholder': 'Enter your mobile no.'}), help_text='<b>Enter your mobile number using your country code</b>, e.g. +254721 ...')
+    course = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mt-2 mb-2'}), choices=CHOICE_COURSE)
+    year = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=CHOICE_YEAR)
+    semester = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=CHOICE_SEMESTER)
+    
+
+    class Meta:
+        model = Students
+        fields = ['gender', 'phone_number', 'course', 'school', 'year', 'semester', 'profile_pic']
+
+class EditProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Students
+        fields = ['profile_pic']
 
